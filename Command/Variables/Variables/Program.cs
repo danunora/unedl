ce414ba529c8d83fd12cbf2a110d4b14f73d2fd6
Daniel Nuno <daniel_nuno@hotmail.com>
2018-09-06 18:27:46 -0500

    Variables

---
+++ Command/Variables/Variables/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte numero1 = 21;
+            int count1 = 10;
+            float totalPrice1 = 20.95f;
+            char Caracter1 = 'A';
+            string firstName1 = "Daniel";
+            string lastName1 = "Nuno";
+            bool isWorking1 = false;
+
+            var numero2 = 22;
+            var count2 = 10;
+            var totalPrice2 = 20.95f;
+            var Caracter2 = 'A';
+            var firstName2 = "Daniel";
+            var lastName2 = "Nuno";
+            var isWorking2 = false;
+
+            Console.WriteLine(numero1);
+            Console.WriteLine(count1);
+            Console.WriteLine(totalPrice1);
+            Console.WriteLine(Caracter1);
+            Console.WriteLine(firstName1);
+            Console.WriteLine(lastName1);
+            Console.WriteLine(isWorking1);
+            Console.WriteLine(numero2);
+            Console.WriteLine(count2);
+            Console.WriteLine(totalPrice2);
+            Console.WriteLine(Caracter2);
+            Console.WriteLine(firstName2);
+            Console.WriteLine(lastName2);
+            Console.WriteLine(isWorking2);
+
+        }
+    }
+}
