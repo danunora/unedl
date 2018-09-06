bb4878d6374391eed68d248cb3e4598686325d83
Daniel Nuno <daniel_nuno@hotmail.com>
2018-09-06 18:05:23 -0500

    initial

---
+++ Command/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Console.WriteLine("This is a test!");
+            checked
+            {
+                byte numero = 255;
+                numero++;
+                Console.WriteLine("Numero: ", numero);
+            }
+
+        }
+    }
+}
