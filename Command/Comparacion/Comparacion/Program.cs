a39988f30bfe092654c0e2aa948e482452d9c4a6
Daniel Nuno <daniel_nuno@hotmail.com>
2018-09-12 09:56:22 -0500

    operadores

---
+++ Command/Comparacion/Comparacion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 2;
+            Console.WriteLine("#### Operadores Comparacion con Integers ####");
+            Console.WriteLine("int a = 10;");
+            Console.WriteLine("int b = 2;");
+            Console.WriteLine("a == b: {0}", a == b);
+            Console.WriteLine("a != b: {0}", a != b);
+            Console.WriteLine("greater than a > b: {0}", a > b);
+            Console.WriteLine("greater or equal to a >= b: {0}", a >= b);
+            Console.WriteLine("less than a < b: {0}", a < b);
+            Console.WriteLine("less than or equal to a <= b: {0}", a <= b);
+            Console.WriteLine("                  ");
+
+        }
+    }
+}
