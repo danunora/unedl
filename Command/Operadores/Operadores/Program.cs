63a35d7bed0375aca4661a9ce9e07a4c2cd74f29
Daniel Nuno <daniel_nuno@hotmail.com>
2018-09-12 19:50:50 -0500

    Operadores

---
+++ Command/Operadores/Operadores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+            int b = 2;
+            Console.WriteLine("#### Operadores Aritmeticos con Integers ####");
+            Console.WriteLine("int a = 10;");
+            Console.WriteLine("int b = 2;");
+            Console.WriteLine("a + b: {0}", a + b);
+//            Console.WriteLine("a + b: {0}", (float) a + (float) b);
+            Console.WriteLine("a - b: {0}", a - b);
+            Console.WriteLine("a * b: {0}", a * b);
+            Console.WriteLine("a / b: {0}", a / b);
+            Console.WriteLine("a % b: {0}", a % b);
+            Console.WriteLine("a++: {0}", a++);
+            Console.WriteLine("++a: {0}", ++a);
+            Console.WriteLine("                  ");
+
+            float d = 23.5f;
+            float e = 12.1f;
+            float f = 0.0f;
+            float h = 0.0f;
+            Console.WriteLine("#### Operadores Aritmeticos con Float ####");
+            Console.WriteLine("float d = 23.5f;");
+            Console.WriteLine("float e = 12.1f;");
+            Console.WriteLine("d + e: {0}", d + e);
+            Console.WriteLine("d - e: {0}", d - e);
+            Console.WriteLine("d * e: {0}", d * e);
+            Console.WriteLine("d / e: {0}", d / e);
+            Console.WriteLine("d % e: {0}", d % e);
+            f = d++;
+            Console.WriteLine("f=d++: {0}", f);
+            h = ++d;
+            Console.WriteLine("h=++d: {0}", h);
+            Console.WriteLine("                  ");
+
+        }
+    }
+}
---
+++ Command/OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Valor1 = true;
+            bool Valor2 = true;
+            Console.WriteLine("Valor1 {0} && Valor2 {1}: {2}",Valor1,Valor2,Valor1 && Valor2);
+            Valor2 = false;
+            Console.WriteLine("Valor1 {0} && Valor2 {1}: {2}", Valor1, Valor2, Valor1 && Valor2);
+
+        }
+    }
+}
