3cd071d546787cf9a01e3bb3ddd7a3fb4fadef25
Daniel Nuno <daniel_nuno@hotmail.com>
2018-09-21 18:20:14 -0500

    matriz

---
+++ Command/RecorridoMatriz/RecorridoMatriz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecorridoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arreglo = new int[4, 5]
+            {
+                {1,9,23,34,10},
+                {2,8,67,56,23},
+                {3,7,84,78,43},
+                {4,5,90,98,34}
+            };
+
+            Console.WriteLine("Tamano del Arreglo: {0}", arreglo.Length);
+
+            for (int i = 0; i <= 4; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    Console.WriteLine("Elemento: {0}", arreglo[j, i]);
+                }
+                
+            }
+        }
+    }
+}
