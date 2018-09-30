acfba4e4303effafdef3cdd19c006dfdaa565c7f
Daniel Nuno <daniel_nuno@hotmail.com>
2018-09-30 15:25:58 -0500

    delegate

---
+++ Command/DelegatePhoto/DelegatePhoto/Photo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePhoto
+{
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            Console.WriteLine("Loading photo");
+            return new Photo();
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saving photo");
+        }
+    }
+}
