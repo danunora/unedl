42f2f177ad289afff292793e2500bef6fe6a67d3
Daniel Nuno <daniel_nuno@hotmail.com>
2018-10-01 00:10:51 -0500

    eventsanddelegates

---
+++ Command/EventsAndDelegates/EventsAndDelegates/Video.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class Video
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Command/EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // 1) Define a delegate
+
+
+        // 2) Define an event based on that delegate
+
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video ...");
+            Thread.Sleep(3000);
+            Console.WriteLine("Video encoded");
+
+        }
+    }
+}
