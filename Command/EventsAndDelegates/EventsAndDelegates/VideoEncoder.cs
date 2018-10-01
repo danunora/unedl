3e7f604bb281c9109b54ab7fd6a797263910f111
Daniel Nuno <daniel_nuno@hotmail.com>
2018-10-01 00:42:28 -0500

    implementation

--- Command/EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
+++ Command/EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -10,9 +10,10 @@ namespace EventsAndDelegates
     public class VideoEncoder
     {
         // 1) Define a delegate
-
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
 
         // 2) Define an event based on that delegate
+        public event VideoEncodedEventHandler VideoEncoded;
 
         public void Encode(Video video)
         {
@@ -20,6 +21,17 @@ namespace EventsAndDelegates
             Thread.Sleep(3000);
             Console.WriteLine("Video encoded");
 
+            OnVideoEncoded();
         }
+
+        // 3) Raise the event
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+            }
+        }
+
     }
 }
