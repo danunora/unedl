3fc2869d1958faedcc85997b11cce33cf75d803e
Daniel Nuno <daniel_nuno@hotmail.com>
2018-10-09 12:08:54 -0500

    namespaces

---
+++ Command/Namespaces/Namespaces/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using primer_namespace;
+using primer_namespace.segundo_namespace;
+
+namespace primer_namespace
+{
+    class Abc
+    {
+        public void func()
+        {
+            Console.WriteLine("primer namespace");
+        }
+    }
+
+    namespace segundo_namespace
+    {
+        class Efg
+        {
+            public void func()
+            {
+                Console.WriteLine("segundo namespace");
+            }
+        }
+    }
+}
+    class testClass
+    {
+        static void Main(string[] args)
+        {
+        Abc pc = new Abc();
+        Efg sc = new Efg();
+        pc.func();
+        sc.func();
+        Console.ReadKey();
+        }
+    }
+
