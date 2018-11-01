e28f5be66e60668373bb201e6e90071a4d0a6a45
Daniel Nuno <daniel_nuno@hotmail.com>
2018-10-31 21:22:38 -0600

    semaphores

---
+++ Command/Semaphores/Semaphores/Program.cs
@@ -0,0 +1,42 @@
+﻿// source: http://www.tutorialspoint.com/Semaphore-in-Chash
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Semaphores
+{
+    class Program
+    {
+        const int VEINTE = 20;
+        static Thread[] myThread = new Thread[VEINTE];
+
+        // initial count, maximum count
+        static Semaphore semaphore = new Semaphore(2, 2);
+
+        // To simulate some activity
+        static void DoSomething()
+        {
+            Console.WriteLine("{0} = waiting", Thread.CurrentThread.Name);
+            semaphore.WaitOne();
+            Console.WriteLine("{0} begins!", Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} releasing...", Thread.CurrentThread.Name);
+            semaphore.Release();
+        }
+
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < VEINTE; j++)
+            {
+                myThread[j] = new Thread(DoSomething);
+                myThread[j].Name = "thread number " + j;
+                myThread[j].Start();
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Command/Signals/Signals/Program.cs
@@ -0,0 +1,34 @@
+﻿// Source: https://jj09.net/multithreading-in-csharp/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Signals
+{
+    class Program
+    {
+            static EventWaitHandle _waitHandle = new AutoResetEvent(false);
+
+            static void Main()
+            {
+                new Thread(Operation).Start();
+                Thread.Sleep(1000);                  // Computation...
+                Console.WriteLine("Wait...");
+                _waitHandle.WaitOne();               // Wait for notification
+                Console.WriteLine("Notified!");
+            }
+
+            static void Operation()
+            {
+                Console.WriteLine("Start operation...");
+                Thread.Sleep(2000);                  // Computation...
+                Console.WriteLine("Operation finished!");
+                _waitHandle.Set();                   // Notify the Waiter
+                Thread.Sleep(1000);                  // Some other computation...
+            }
+        }
+    }
