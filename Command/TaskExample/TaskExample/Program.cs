6a0dcff48f8fe69a57a333cd5c393c0f93b7a0f1
Daniel Nuno <daniel_nuno@hotmail.com>
2018-11-01 18:08:00 -0600

    task

---
+++ Command/TaskExample/TaskExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    class Program
+    {
+        public static void Write(char c)
+        {
+            int i = 1000;
+
+            while(i --> 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Write('.'));
+
+            var t = new Task(() => Write('?'));
+            t.Start();
+                       
+            Write('-');
+            Console.WriteLine("Main program done");
+            Console.ReadKey();
+        }
+    }
+}
