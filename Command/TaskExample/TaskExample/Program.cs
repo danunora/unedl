8e2b4e019d5eda6d400dd2307fd7fa1ef0cc28d9
Daniel Nuno <daniel_nuno@hotmail.com>
2018-11-02 13:11:19 -0600

    task3

--- Command/TaskExample/TaskExample/Program.cs
+++ Command/TaskExample/TaskExample/Program.cs
@@ -28,18 +28,44 @@ namespace TaskExample
             }
         }
 
-        static void Main(string[] args)
+        public static int TextLength(object o)
+        {
+            Console.WriteLine($"\nTask with id {Task.CurrentId} processing object {o}...");
+            return o.ToString().Length;
+        }
+
+        public static void Test1()
         {
             // Test 1 using Write (char)
             Task.Factory.StartNew(() => Write('.'));
             var t1 = new Task(() => Write('?'));
             t1.Start();
             Write('-');
+        }
 
+        public static void Test2()
+        {
             // Test 2 using Write (object)
             var t2 = new Task(Write, "hello");
             t2.Start();
             Task.Factory.StartNew(Write, 123);
+        }
+
+        static void Main(string[] args)
+        {
+
+            //Test1();
+            //Test2();
+            
+            // Test 3 
+            string text1 = "testing", text2 = "this";
+            var task1 = new Task<int>(TextLength, text1);
+            task1.Start();
+
+            Task<int> task2 = Task.Factory.StartNew<int>(TextLength, text2);
+
+            Console.WriteLine($"\nLenght of '{text1}' is {task1.Result}");
+            Console.WriteLine($"Lenght of '{text2}' is {task2.Result}");
 
             Console.WriteLine("Main program done");
             Console.ReadKey();
