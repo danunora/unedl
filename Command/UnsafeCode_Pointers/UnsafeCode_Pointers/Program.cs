d69458bb1b8ed82f9da1e4a32db466fc436a88e9
Daniel Nuno <daniel_nuno@hotmail.com>
2018-11-13 23:35:12 -0600

    unsafe & pointers

---
+++ Command/UnsafeCode_Pointers/UnsafeCode_Pointers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsafeCode_Pointers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pointers1();
+            Console.ReadKey();
+            Pointers2();
+        }
+
+        unsafe private static void Pointers2()
+        {
+            int var = 100;
+            int* p = &var;
+            Console.WriteLine("var: {0}", var);
+            Console.WriteLine("p->ToString(): {0}", p->ToString());
+            Console.WriteLine("Address: {0}",(int)p);
+        }
+
+        unsafe private static void Pointers1()
+        {
+            fixed (char* valor = "Hello World !\n")
+            {
+                char* ptr = valor;
+                while (*ptr != '\0')
+                {
+                    Console.Write(*ptr);
+                    ++ptr;
+                }
+            }
+        }
+    }
+}
